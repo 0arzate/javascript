ff873ef3749dee1cdbe7b8cbd3c2962c6a47291d
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-08-15 07:27:13 -0500

    💫 var number added

--- src/js/var.js
+++ src/js/var.js
@@ -1,7 +1,17 @@
-var age = 24, name = 'John', texto = 'Developer';
+var age = 24,
+	name = 'John',
+	texto = 'Developer'
+
+var number = 10,
+	numberStr = '10',
+	six = 2,
+	pi = 3.141592
 
 age = 'age 24'
+six -= 6
 
-
-
-console.log(texto.substring(0, 3));
+console.log('String', texto.substring(0, 3))
+console.log('NumberStr', parseFloat(numberStr))
+console.log('Six', six)
+console.log('Pi', pi.toFixed(2))
+console.log('Div', 10 / 2)
