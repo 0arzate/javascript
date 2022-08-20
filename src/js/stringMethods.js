d7b86df8099fc0fdd3ae73704e0bea07994534d4
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-08-19 21:18:10 -0500

    💫 string methods added

--- src/js/stringMethods.js
+++ src/js/stringMethods.js
@@ -9,3 +9,12 @@ console.log('substring: ' + Boolean(str.substr(11, 11)))
 console.log('charAt: ' + str.charAt(5))
 console.log('repeat: ' + str.repeat(2))
 console.log('split: ' + str.split(' '))
+console.log('startsWith: ' + str.startsWith('Hola'))
+console.log('endsWith: ' + str.endsWith('ndo'))
+console.log('search: ' + str.search('dos'))
+console.log('toLowerCase: ' + str.toLowerCase())
+console.log('toUpperCase: ' + str.toUpperCase())
+console.log('replace: ' + str.replace('Hola', 'Adios'))
+
+const str2 = ' Hola mundo '
+console.log('trim: ' + str2.trim())
