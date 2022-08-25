66238d83c71d97e4336ebdb73f9fd5e8e04006cc
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-08-24 22:00:27 -0500

    💫 added to fixed method

--- src/js/numbersMethods.js
+++ src/js/numbersMethods.js
@@ -1,6 +1,7 @@
 var number = 100
-var numberString = '100'
-var numberFloat = 3.1416
+var numberString = '110.200'
+var numberFloat = 2.203
 
 console.log('toString: ', typeof numberString.toString())
-console.log('toFixed: ', typeof numberFloat.toFixed(2))
+console.log('toFixed: ', numberFloat.toFixed(0))
+console.log('parseInt: ', parseInt([numberString, 6]))
