fddecbfcb28f4d4cf942eb2abbedb26670871124
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2023-07-23 22:25:52 -0600

    ✨ add closure

---
+++ src/javascript/closure.js
@@ -0,0 +1,16 @@
+const counter = () => {
+  let number = 0
+  return (amount = 0) => {
+    return (number += amount)
+  }
+}
+
+const newCounter = counter()
+newCounter(10)
+newCounter(5)
+console.log(newCounter(10))
+
+const otherCounter = counter()
+otherCounter(5)
+otherCounter(10)
+console.log(otherCounter())
