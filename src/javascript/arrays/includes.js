f10147b88deee571d502f4f05122bde1476007ec
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2024-01-16 21:23:42 -0600

    ✨ add includes arrays

---
+++ src/javascript/arrays/includes.js
@@ -0,0 +1,14 @@
+const numbers = [1, 2, 3, 4, 10, 11]
+const jose = { name: 'Jose', adreess: { city: 'CDMX' } }
+const pedro = { name: 'Pedro', adreess: { city: 'NL' } }
+
+const objects = [jose, pedro]
+
+const strings = ['Jose', 'Perez', 'Osavaldo', 'marlon']
+
+const booleans = [true]
+
+console.log(numbers.includes(2))
+console.log(objects.includes(jose))
+console.log(strings.includes('jose'))
+console.log(booleans.includes(false))
