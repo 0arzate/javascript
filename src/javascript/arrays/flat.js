a8a3ab225a36ea3d5dc60234d00c2c488cd4b10d
osvarz.new <osvarz.new@gmail.com>
2024-01-19 00:49:59 +0000

    ✨ update flat method

--- src/javascript/arrays/flat.js
+++ src/javascript/arrays/flat.js
@@ -1,4 +1,8 @@
-const myArray = [1, 2, [3, 4, [5, 6, [7]]], [8, 9]]
+const arrayOneLevel = [1, 2, 3, 4, [5, 6, 7]]
+const arrayTwoLevel = [1, 2, [3, 4, [5, 6, 7]]]
+const arrayInfinity = [1, 2, [3, 4, [5, 6, [7]]], [8, 9]]
 
-console.log(myArray.flat(3))
-console.log(myArray)
+console.log(arrayOneLevel.flat(1))
+console.log(arrayOneLevel.flat(2))
+console.log(arrayInfinity.flat(Infinity))
+
