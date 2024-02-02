3aa00839e824ebed8cc0d853cc3d51da58f2bc72
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2024-02-01 21:43:50 -0600

    ✨ add mokepon file

---
+++ src/javascript/mokepon.js
@@ -0,0 +1,6 @@
+function seleccionarMascota() {
+  alert('SELECIONASTE TU MASCOTA')
+}
+
+let botonMascota = document.getElementById('boton-mascota')
+botonMascota.addEventListener('click', seleccionarMascota)
