9d9778abdd409f823bc384f0e318724ac8869b8b
osvarz.new <osvarz.new@gmail.com>
2024-06-27 13:05:08 +0000

    ✨ add unshift method

--- src/dataStructures/customArray.js
+++ src/dataStructures/customArray.js
@@ -1,4 +1,4 @@
-export class CustomArray {
+class CustomArray {
 	constructor() {
 		this.length = 0
 		this.data = {}
@@ -13,4 +13,47 @@ export class CustomArray {
 		this.length++
 		return this.length
 	}
+
+	pop(index) {
+		delete this.data[this.length]
+	}
+
+	unshift(newItem) {
+		if(!newItem) {
+			return this.length
+		}
+
+		for(let i = this.length; i > 0; i--) {
+			const currentValue = this.data[i - 1]
+			const newIndex = i
+
+			this.data[newIndex] = currentValue
+		}
+
+		this.data[0] = newItem
+		this.length++
+		return this.length
+	}
+
+	shift(){
+		if(this.length === 0) {
+			return undefined
+		}
+
+    delete this.data[0]
+		this.length--
+		console.log({ data: this.data })
+		for (let index = 0; index <= this.length; index++) {
+			const currentValue = index + 1
+			this.data[index] = this.data[currentValue]
+		}
+  }
 }
+
+const myArray = new CustomArray()
+
+myArray.unshift("!!!")
+myArray.unshift("Platzinauta")
+myArray.unshift("lograste")
+myArray.unshift("lo")
+console.log(myArray)
